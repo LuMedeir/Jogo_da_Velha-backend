77f903ae8e09358bcf6da5be41efc2748e3c9d6d
Luiza Medeiros <medeirosluiza05@gmail.com>
2024-12-16 09:47:21 -0300

    implementação de status de erro nos controladores

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,18 +1,21 @@
 class GamesController < ActionController::API
 
+  # Captura cada exceção ActiveRecord::RecordNotFound(quando o id não é econtrado no banco de dados) e executa o método `record_not_found` como erro padrão
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   def create
     game = Game.create(game_state: Array.new(9, ""), winner: "")
-    render json: game
+    render json: game, status: :created # Código 201
   end
 
   def index
     games = Game.all
-    render json: games
+    render json: games, status: :ok # Código 200
   end
 
   def show
     game = Game.find(params[:id])
-    render json: game
+    render json: game, status: :ok # Código 200
   end
 
   def move
@@ -23,32 +26,29 @@ class GamesController < ActionController::API
     if @game.game_state[position].blank? && @game.winner.blank?
       @game.game_state[position] = player
       @game.save
-      @game.check_winner(player) 
+      @game.check_winner(player)
     end
-
-    render json: @game
+    render json: @game, status: :ok # Código 200
   end
 
   def destroy
     game = Game.find(params[:id])
     game.destroy
-    render json: game
+    render json: game, status: :ok # Código 200
   end
 
   def reset
     @game = Game.find(params[:id])
 
-    @game.update(game_state: Array.new(9, ""))  
-    @game.update(winner: "")
-    render json: @game
+    @game.update(game_state: Array.new(9, ""), winner: "")
+    render json: @game, status: :ok # Código 200
   end
 
   def update
     @game = Game.find(params[:id])
 
     @game.update(game_params)
-    render json: @game
-
+    render json: @game, status: :ok # Código 200
   end
 
   private
@@ -56,6 +56,11 @@ class GamesController < ActionController::API
   # Define os parâmetros permitidos para o update
   def game_params
     params.require(:game).permit(:winner, game_state: [])
+  end
+
+  # Define o erro padrão para o RecordNotFound
+  def record_not_found
+    render json: { error: "Game not found" }, status: :not_found # Código 404
   end
 
 end
---
+++ spec/models/spec_controllers.rb
@@ -0,0 +1,3 @@
+# Teste de controladores
+
+require 'rails_helper'
